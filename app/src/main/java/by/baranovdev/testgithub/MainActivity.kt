3573f045cbc9206e5d7dfa3230928cb88a9e11d7
Ilya Baranov <baranovdev.by@gmail.com>
2024-09-09 01:59:21 +0300

    ui changes

--- app/src/main/java/by/baranovdev/testgithub/MainActivity.kt
+++ app/src/main/java/by/baranovdev/testgithub/MainActivity.kt
@@ -1,8 +1,12 @@
 package by.baranovdev.testgithub
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import by.baranovdev.testgithub.application.TestApplication
@@ -19,6 +23,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        window.statusBarColor = resources.getColor(R.color.white)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = window.insetsController
+            if (controller != null) {
+                controller.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                )
+            }
+        } else {
+            // Для более старых версий Android
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
     }
 }
---
+++ app/src/main/java/by/baranovdev/testgithub/utils/Utils.kt
@@ -0,0 +1,3 @@
+package by.baranovdev.testgithub.utils
+
+fun toPx(dp:Int, scale: Float) = ( dp.toFloat() * scale + 0.5f).toInt()
